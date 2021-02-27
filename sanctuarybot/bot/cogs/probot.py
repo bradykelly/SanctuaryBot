2e0d96a1f89115c4c934296a47d2e594b3701166
Brady Kelly <brady@bradykelly.net>
2021-02-27 18:58:01 +0200

    Resuming work after short break

--- sanctuarybot/bot/cogs/probot.py
+++ sanctuarybot/bot/cogs/probot.py
@@ -8,14 +8,14 @@ from sanctuarybot.bot.basecog import BaseCog
 from sanctuarybot.utils.probot import ProBotUtils
 
 
-PROBOT_COMMANDS = ["read", "clear"]
+PROBOT_COMMANDS = ["import", "clear"]
 
 class Probot(BaseCog):
     """Commands to parse output from the ProBot leaderboards"""
 
     def __init__(self, bot):
         self.bot = bot
-        self.probot = ProBotUtils()
+        self.probot = ProBotUtils(bot)
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -26,12 +26,12 @@ class Probot(BaseCog):
         name="probot",
         aliases=["pb", "pbot"],
         brief="Commands to work with Probot",
-        usage="read [read_count] | clear [clear_count]")
+        usage="import [read_count] | clear [clear_count]")
     async def probot_group(self, ctx):
         
         # This condition doesn't automatically raise CommandNotFound, thus doesn't trigger @probot_group.error
         if ctx.subcommand_passed is not None and not ctx.subcommand_passed in PROBOT_COMMANDS:
-            await self.output.show_message_embed(ctx, await self.output.format_usage(ctx), "Usage")
+            raise CommandNotFound
 
     @probot_group.error
     async def on_probot_error(self, ctx, error):
@@ -39,19 +39,20 @@ class Probot(BaseCog):
             await self.output.show_message_embed(ctx, await self.output.format_usage(ctx), "Usage")
 
     @probot_group.command(
-        name="read",
-        aliases=["rd"],
-        usage="[count]"
+        name="import",
+        aliases=["imp", "ip"],
+        usage="[read_count]"
     )
-    async def read_command(self, ctx, count: int=None):
+    async def import_command(self, ctx, count: int=None):
         channel_messages = await self.probot.read_message_history(ctx, count)
-        count = await self.probot.import_probot_messages(ctx, channel_messages)
-        await self.output.show_message_embed(ctx, f"{count} ProBot leaderboard messages parsed", "read")
+        embed_count = await self.probot.import_probot_leaderboards(ctx, channel_messages)
+        await self.output.show_message_embed(ctx, f"{embed_count} ProBot leaderboard messages imported", "import")
 
-    @read_command.error
+    @import_command.error
     async def on_clear_error(self, ctx, error):
         await self.output.show_message_embed(ctx, 
             f"Command failed: {error.message if hasattr(error, 'message') else str(error)}", "clear")
+
 
     @probot_group.command(
         name="clear",
@@ -59,7 +60,6 @@ class Probot(BaseCog):
         usage="[count]"
     )
     async def clear_command(self, ctx, count=None):        
-        #TODO Get channel message count
         messages = await self.probot.read_message_history(ctx, count)
         await ctx.send(f"Clearing {len(messages)} messages...")
         count = await self.probot.delete_messages(ctx, count)
